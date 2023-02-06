7734eed9f4ffb441b969b88554efb131ae76d740
Razael001 <77176187+Razael001@users.noreply.github.com>
2023-02-06 11:56:05 +0100

    first commit

--- uniswap.js
+++ uniswap.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 const { Network, Alchemy } = require("alchemy-sdk");
 
 const settings = {
-  apiKey: "JOHdJUN4I8RqCltE8B7e5BQBtucmGX0G",
+  apiKey: "KinoxPmmbfEPF81q83UIWQWrRRzVWABM",
   network: Network.ETH_MAINNET,
 };
 const alchemy = new Alchemy(settings);
@@ -53,7 +53,6 @@ async function getUserData() {
 
   // Create a set to store unique transactions
   const uniqueTransactions = new Set();
-
   // Loop through each liquidity position
   liquidityPositions.forEach(async (position) => {
     // If the user has a non-zero balance in the liquidity token
@@ -72,93 +71,112 @@ async function getUserData() {
       });
 
       // Loop through each transaction
-      res.transfers.forEach(async (txn) => {
-        // If the transaction is unique
-        if (!uniqueTransactions.has(txn.hash)) {
-          // Add the transaction hash to the set of unique transactions
-          uniqueTransactions.add(txn.hash);
+      var bal = 0;
+      var bal2 = 0;
 
-          // Get the hash of the transaction
-          const txnHash = txn.hash;
-
-          // Make a POST request to get the mint data
-          const mintResponse = await axios.post(
-            "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2",
-            {
-              query: `{
-            mint(id: "${txnHash}-0") {
-              timestamp
-              amount0
-              amount1
-            }
-            
-          }`,
-            }
-          );
-          // Get the mint data
-          const mint = mintResponse.data.data.mint;
-          // If the mint data exists
-          if (mint) {
-            const timestamp = mint.timestamp;
-
-            const day = Math.floor(timestamp / 86400);
-            const response0 = await axios.post(
+      for (let i = 0; i < res.transfers.length; i++) {
+        async function processTransactions() {
+          let txn = res.transfers[i];
+          // If the transaction is unique
+          if (!uniqueTransactions.has(txn.hash)) {
+            // Add the transaction hash to the set of unique transactions
+            uniqueTransactions.add(txn.hash);
+            const txnHash = txn.hash;
+            const mintResponse = await axios.post(
               "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2",
               {
                 query: `{
+              mint(
+                id: "${txnHash}-0"
+              ) {
+                amount0
+                amount1
+              }
+            }`,
+              }
+            );
+            // Get the mint data
+            const mint = mintResponse.data.data.mint;
+            // If the mint data exists
+            if (mint) {
+              const timestamp = mint.timestamp;
+
+              const day = Math.floor(timestamp / 86400);
+              const response0 = await axios.post(
+                "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2",
+                {
+                  query: `{
                  tokenDayData(id: "${position.pair.token0.id}-${day}")  {
                   priceUSD
                 }
                  
               }`,
-              }
-            );
-            const response1 = await axios.post(
-              "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2",
-              {
-                query: `{
+                }
+              );
+              const response1 = await axios.post(
+                "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2",
+                {
+                  query: `{
                  tokenDayData(id: "${position.pair.token1.id}-${day}")  {
                   priceUSD
                 }
                  
               }`,
+                }
+              );
+              //  const priceUSD = response0.data.data.tokenDayData.priceUSD;
+              //  const priceUSD1 = response1.data.data.tokenDayData.priceUSD;
+
+              // Convert the timestamp to a human-readable date and time
+              const currentTime = new Date();
+              const timed = new Date(timestamp * 1000);
+              const difference = currentTime - timed;
+              const time = difference / (365.25 * 24 * 60 * 60 * 1000);
+              // Calculate the final token0 price
+              const token0price =
+                position.pair.token1Price * position.pair.token0Price;
+
+              function totalReturn(initialInvestment = (mint.amount0 * 1), initialPrice1 = position.pair.token0Price, initialPrice2 = position.pair.token0Price, currentPrice1 = position.pair.token1Price, currentPrice2 = token0price.toFixed(), amount1 = mint.amount0, amount2 = mint.amount1, timeSinceInvestment = 1.5) {
+                const L = Math.sqrt(amount1 * amount2);
+                const L_0 = Math.sqrt(position.pair.token0Price * position.pair.token1Price);
+                const α = ((currentPrice1 * currentPrice2) / (initialPrice1 * initialPrice2))
+                const C = initialInvestment;
+                const tradingFeesReturn = Math.exp(α * timeSinceInvestment) - 1;
+                const priceVariationReturn = Math.sqrt(currentPrice2 / initialPrice2) - 1;
+                const impermanentLoss = 1 - L / (amount1 * currentPrice1 + amount2 * currentPrice2);
+
+                return ((C) + (tradingFeesReturn * 1) + (priceVariationReturn * 1) - impermanentLoss)
               }
-            );
+              bal += totalReturn();
+              if (txn[i] == res.transfers[res.transfers.length]) {
+                console.log(bal)
+              }
+              else {}
 
-            const priceUSD = response0.data.data.tokenDayData.priceUSD;
-            const priceUSD1 = response1.data.data.tokenDayData.priceUSD;
+              function totalReturn2(initialInvestment = (mint.amount1 * 1), initialPrice1 = position.pair.token0Price, initialPrice2 = position.pair.token0Price, currentPrice1 = position.pair.token1Price, currentPrice2 = token0price.toFixed(), amount1 = mint.amount0, amount2 = mint.amount1, timeSinceInvestment = 1.5) {
+                const L = Math.sqrt(amount1 * amount2);
+                const L_0 = Math.sqrt(position.pair.token0Price * position.pair.token1Price);
+                const α = ((currentPrice1 * currentPrice2) / (initialPrice1 * initialPrice2))
+                const C = initialInvestment;
+                const tradingFeesReturn = Math.exp(α * timeSinceInvestment) - 1;
+                const priceVariationReturn = Math.sqrt(currentPrice2 / initialPrice2) - 1;
+                const impermanentLoss = 1 - L / (amount1 * currentPrice1 + amount2 * currentPrice2);
 
-            // Convert the timestamp to a human-readable date and time
-            const time = new Date(timestamp * 1000).toLocaleString();
-            // Calculate the final token0 price
-            const token0price =
-              position.pair.token1Price * position.pair.token0Price;
-
-            // Log the transaction data to the console
-            console.log("Transaction hash: ", txnHash);
-            console.log("Token0 price on the day of deposit: ", priceUSD);
-            console.log("Token1 price on the day of deposit: ", priceUSD1);
-            console.log("Time: ", time);
-            console.log(
-              "Token0 amount: ",
-              mint.amount0,
-              position.pair.token0.symbol
-            );
-            console.log("token0 per token1 price:", position.pair.token0Price);
-            console.log("current token0Price:", position.pair.token1Price);
-            console.log(
-              "Token1 amount: ",
-              mint.amount1,
-              position.pair.token1.symbol
-            );
-            console.log("token1 per token0 price:", position.pair.token1Price);
-            console.log("current token1Price:", token0price.toFixed());
-            console.log("pool address:", id);
-            console.log(" ");
-            console.log(" ");
+                return ((C) + (tradingFeesReturn * 1) + (priceVariationReturn * 1) - impermanentLoss)
+              }
+              bal2 += totalReturn2();
+              if (i = res.transfers.length) {
+                console.log(bal2)
+              }
+              else {}
+            }
           }
+          return bal;
         }
-      });
+        processTransactions()
+      };
+      console.log("Total balance of second Token " + bal2)
+      console.log("Total balance of first Token " + bal)
     }
   });
 }
